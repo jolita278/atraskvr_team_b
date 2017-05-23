7861d4d2f2538cd94254d9438502083ec174b118
don79is <don79is@gmail.com>
2017-05-23 15:31:54 +0300

    add lang categories  models seeders

---
+++ app/Models/VRCategories.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class VRCategories extends CoreModel
+{
+    /**
+     * Table name
+     * @var string
+     */
+    protected $table = 'vr_categories';
+
+    /**
+     * Fields which will be manipulated
+     * @var array
+     */
+    protected $fillable = ['id'];
+}
