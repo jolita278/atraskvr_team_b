cee73342c2bc4f7c164e47ace8d4003859cabb93
JolitaAG <jolita278@gmail.com>
2017-05-26 15:27:46 +0300

    added url for categories and languages

--- resources/views/admin/adminSideB.blade.php
+++ resources/views/admin/adminSideB.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid" style="background-color: yellow">
         <h1>Admin  side bar</h1>
     <a href="#">Pages</a><br/>
-    <a href="#">Categories</a><br/>
+    <a href="{{ url('/admin/categories/')}}">Categories</a><br/>
     <a href="{{ url('/admin/upload/')}}">Resources</a><br/>
     <a href="#">Orders</a><br/>
     <a href="{{ url('/admin/users/')}}">Users</a><br/>
-    <a href="#">Languages</a><br/>
+    <a href="{{ url('/admin/languages/')}}">Languages</a><br/>
     <a href="#">Menus</a><br/>
     </div>
 </div>
