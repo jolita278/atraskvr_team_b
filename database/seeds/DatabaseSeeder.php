466ca687ba4da6ee5e27d8859eba207b8f825b22
edith321 <edita.paulikaite@inbox.lt>
2017-05-28 22:46:44 +0300

    Created pages translations seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoriesSeeder::class);
         $this->call(CategoriesTranslationsSeeder::class);
         $this->call(PagesSeeder::class);
+        $this->call(PagesTranslationsSeeder::class);
 
     }
 }
