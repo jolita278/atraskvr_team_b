ca3e08796e2226cc3a6c1dcfc0f655e75cad74ef
Jolita <jolita278@gmail.com>
2017-05-30 22:41:18 +0300

    edited-translated blades

--- resources/views/admin/adminMenusEdit.blade.php
+++ resources/views/admin/adminMenusEdit.blade.php
@@ -2,28 +2,28 @@
 
 @section('adminMenusCreate')
     <div class="container">
-        <h2>Sukurti naują meniu įrašą</h2>
+        <h2>Edit menu</h2>
 
         {!! Form::open(['url' => route($edit, $item['id'])]) !!}
         <br>
-        {{ Form::label('sequence', 'Eiliškumas')}}<br>
+        {{ Form::label('sequence', 'Sequence')}}<br>
         {{Form::text('sequence', $item['sequence'])}}
         <br>
-        {{ Form::label('parent', 'Tėvinis meniu')}}<br>
+        {{ Form::label('parent', 'Parent')}}<br>
         {{Form::text('parent', $item['parent'])}}
         <br>
-        {{ Form::label('title', 'Pavadinimas')}}<br>
+        {{ Form::label('title', 'Title')}}<br>
         {{Form::select('title', $pages)}}
         <br>
-        {{ Form::label('slug', 'Nuoroda')}}<br>
+        {{ Form::label('slug', 'Slug')}}<br>
         {{Form::text('slug', $item['slug'])}}
         <br>
-        {{ Form::label('language', 'Kalba')}}<br>
+        {{ Form::label('language', 'Language')}}<br>
         {{Form::select('language', $languages)}}
         <br>
         <br>
 
-        {{ Form::submit('Patvirtinti') }} {{--TODO:: button reset--}}
+        {{ Form::submit('Submit') }} {{--TODO:: button reset--}}
 
         {!! Form::close() !!}
     </div>
