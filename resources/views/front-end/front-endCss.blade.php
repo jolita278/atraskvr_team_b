7cb311ce3fc054478af7250b4666a430c4764f0e
Edita <edita.paulikaite@inbox.lt>
2017-06-02 11:46:27 +0300

    fixed menu

--- resources/views/front-end/front-endCss.blade.php
+++ resources/views/front-end/front-endCss.blade.php
@@ -1,4 +1,6 @@
 <link href="/css/app.css" rel=stylesheet>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link href="/css/front-end.css" rel="stylesheet">
+<link href="/css/1024.css" rel="stylesheet">
+<link href="/css/640.css" rel="stylesheet">
 
