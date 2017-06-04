0436886a19b9841955c4290f22bac6bb6fc013e4
edith321 <edita.paulikaite@inbox.lt>
2017-06-04 12:44:51 +0300

    tried to do menu from DB

--- app/Http/Controllers/VRWelcomePageController.php
+++ app/Http/Controllers/VRWelcomePageController.php
@@ -7,11 +7,11 @@ use Illuminate\Routing\Controller;
 class VRWelcomePageController extends Controller {
 
 	public function displayMenu() {
-	    $data['menu_lt'] = VRMenusTranslations::where('language_id', 'lt')->get()->toArray();
-        $data['menu_en'] = VRMenusTranslations::where('language_id', 'en')->get()->toArray();
-        $data['menu_ru'] = VRMenusTranslations::where('language_id', 'ru')->get()->toArray();
+	    //$data['menu_lt'] = VRMenusTranslations::where('language_id', 'lt')->get()->toArray();
+        //$data['menu_en'] = VRMenusTranslations::where('language_id', 'en')->get()->toArray();
+        //$data['menu_ru'] = VRMenusTranslations::where('language_id', 'ru')->get()->toArray();
 
-        //$data['menu_lt'] = VRMenus::with('translations')->get()->toArray();
+        $data['menu_lt'] = VRMenus::with('translations')->get()->toArray();
 
         return view('welcome', $data);
     }
