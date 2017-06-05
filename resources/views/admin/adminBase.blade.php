6799d8cd215ffa543b6bf07b68adebc6630caeca
edith321 <edita.paulikaite@inbox.lt>
2017-06-05 23:51:03 +0300

    started redoing admin front-end

--- resources/views/admin/adminBase.blade.php
+++ resources/views/admin/adminBase.blade.php
@@ -5,34 +5,35 @@
     @include('admin.adminCSS')
 </head>
 <body>
+    <nav>
+        @include('admin.adminNavbar')
+    </nav>
+    <aside>
+        @include('admin.adminSideBar')
+    </aside>
+    <main>
+        {{--@yield('pages')--}}
+        @yield('adminList')
+        @yield('adminResourcesList')
+        @yield('adminUpload').
+        @yield('adminSingle')
+        @yield('adminUsersEdit')
+        @yield('adminPagesCreate')
+        @yield('adminPagesEdit')
 
-@include('admin.adminNavbar')
-@include('admin.adminSideBar')
+        @yield('adminPagesSingle')
+        @yield('adminMenusSingle')
 
-{{--@yield('pages')--}}
-@yield('adminList')
-@yield('adminResourcesList')
-@yield('adminUpload').
-@yield('adminSingle')
-@yield('adminUsersEdit')
-@yield('adminPagesCreate')
-@yield('adminPagesEdit')
-
-@yield('adminPagesSingle')
-@yield('adminMenusSingle')
-
-@yield('adminMenusCreate')
-@yield('adminMenusEdit')
-
-
-@yield('adminPagesList')
-@yield('adminOrdersList')
-
-<footer style="width: 100%">
-    @include('footer')
-</footer>
+        @yield('adminMenusCreate')
+        @yield('adminMenusEdit')
 
 
+        @yield('adminPagesList')
+        @yield('adminOrdersList')
+    </main>
+    <footer style="width: 100%">
+        @include('footer')
+    </footer>
 </body>
 @include('admin.adminJS')
 @yield('scripts')
