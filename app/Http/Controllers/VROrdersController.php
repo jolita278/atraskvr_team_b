5dd0b4a2706fd8efad97e0b2b6d1fff9bddfb5ef
Jolita <jolita278@gmail.com>
2017-06-06 13:26:38 +0300

    orders controller edited

--- app/Http/Controllers/VROrdersController.php
+++ app/Http/Controllers/VROrdersController.php
@@ -1,7 +1,12 @@
 <?php namespace App\Http\Controllers;
 
 use App\Models\VROrders;
+use App\Models\VROrdersReservations;
+use App\Models\VRPages;
+use Carbon\Carbon;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class VROrdersController extends Controller
 {
@@ -14,15 +19,19 @@ class VROrdersController extends Controller
      */
     public function adminIndex()
     {
-        // $configuration = $this->getRoutesData();
+        $configuration = $this->getRoutesData();
         $configuration ['listName'] = 'Orders list';
-        $configuration ['list'] = VROrders::with(['orderReservations'])->get()->toArray();
+        $configuration ['list'] = VROrders::/*with(['orderReservations'])->*/get()->toArray();
         $configuration ['ignore'] = '';
         $configuration ['url'] = url('admin/orders/create');
+        $configuration ['orderReservations'] = 'orderReservations';
 
         return view('admin.adminList', $configuration);
     }
-
+    /**
+     * Get routes data
+     * @return array
+     */
     public function getRoutesData()
     {
         $configuration = [];
@@ -37,9 +46,44 @@ class VROrdersController extends Controller
      *
      * @return Response
      */
-    public function create()
+    public function adminCreate()
     {
-        return view('users.usersOrders');
+        $configuration = [];
+        $date = [];
+        for($days = Carbon::createFromDate();
+            $days->lte(Carbon::createFromDate()->addDays(14));
+            $days->addDay()) {
+            $date[] = $days->format('Y-m-d');
+        }
+        $configuration['date'] = array_combine($date,$date);
+
+        $configuration['experiences'] = DB::table('vr_pages')
+            ->join('vr_pages_translations', 'vr_pages.id', '=', 'vr_pages_translations.page_id')
+            ->select('vr_pages.id', 'vr_pages_translations.title')
+            ->where('vr_pages.category_id', '=','patirciu-kambariai' )
+            ->where('vr_pages_translations.language_id', '=', 'en')->pluck('title', 'id')->toArray();
+
+        $times = [];
+        for($hours = Carbon::createFromTime(10, 00, 00, 'Europe/Vilnius');
+            $hours->lte(Carbon::createFromTime(21, 50, 00, 'Europe/Vilnius'));
+            $hours->addMinutes(10)) {
+            $times[] = $hours->format('H:i');
+        }
+
+        $configuration['times'] = array_combine($times,$times);
+//dd($configuration);
+//        $dateAndTimeArray = [];
+//        foreach ($date as $key => $day) {
+//            $key = [];
+//            foreach ($times as $hour) {
+//                $key [] = $hour;
+//            }
+//            $dateAndTimeArray[$day] = $key;
+//        }
+//
+//        $configuration['dateAndTimeArray'] = $dateAndTimeArray;
+
+        return view('users.usersOrders', $configuration);
     }
 
     /**
@@ -48,9 +92,21 @@ class VROrdersController extends Controller
      *
      * @return Response
      */
-    public function store()
+    public function adminStore()
     {
-        //
+        $data = request()->all();
+       dd($data);
+        $record = VROrders::create(array(
+            'user_id' => Auth::user()->id,
+        ));
+//TODO: foreach
+            VROrdersReservations::create(array(
+                'order_id' => $record->id,
+                'datetime' => $data['the_lab_datetime'],
+                'page_experience_id' => $data['page_experience_id'],
+            ));
+
+        return redirect('/admin/orders/')->with('message', 'Successfully created!');
     }
 
     /**
@@ -98,7 +154,10 @@ class VROrdersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        VROrdersReservations::destroy(VROrdersReservations::where('order_id', $id)->pluck('id')->toArray());
+        VROrders::destroy($id);
+
+        return json_encode(["success" => true, "id" => $id]);
     }
 
 }
