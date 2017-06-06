22972c0b6155152b7c395f088243ba89e7ae6a5c
Jolita <jolita278@gmail.com>
2017-06-06 13:26:57 +0300

    orders blade edited

--- resources/views/users/usersOrders.blade.php
+++ resources/views/users/usersOrders.blade.php
@@ -7,21 +7,26 @@
 
         {!! Form::open(['url' => route('app.admin.orders.create')]) !!}
         <h4><b>Please choose the date</b></h4><br>
+
+        {{ Form::select('date[]', $date, null) }} <br>
         <br>
         <div class="row">
             <div class="col-lg-12">
+            {{--{{dd($experiences)}}--}}
             {{ Form::label('experience_id','Please choose experience') }}<br>
             {{ Form::select('experience_id', $experiences, null) }} <br>
 
         </div>
 
         <div class="row">
+            <div><b>Please choose the time for experience</b></div>
         </div>
         <div class="row">
             <br>
             <div class="col-lg-8">
                 {{--{{dd($time)}}--}}
                 @foreach($times as $key => $value)
+                    {{Form::checkbox('time[]', $key )}}{{$value}}
                     <br/>
                 @endforeach
             </div>
